f0ef363fa8fb6efab4fcb182363906e376695432
Marcel Goldstein <marcel.goldstein@outlook.com>
2019-06-29 21:13:41 +0200

    initial project creation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PegSolitaire
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
